ce5ef7027f0fff3dc10452e2eaef081cd143fb54
Nishant Paliwal <paliwalnishant0@gmail.com>
2025-11-07 11:14:36 +0530

    History page done

--- src/pages/HomeTabs.tsx
+++ src/pages/HomeTabs.tsx
@@ -8,12 +8,11 @@ import {
   IonLabel,
 } from "@ionic/react";
 import { Route, Redirect } from "react-router-dom";
-import { homeOutline, documentOutline, personOutline } from "ionicons/icons";
+import { homeOutline, documentOutline, personOutline, logOutOutline } from "ionicons/icons";
 
 import Homepage from "./Home";
-import History from "./History";
+import History from "./History/History";
 import Profile from "./Profile";
-import Header from "../components/common/Header";
 
 interface TabsProps {
   onLogout: () => void;
@@ -21,49 +20,35 @@ interface TabsProps {
 
 const HomeTabs: React.FC<TabsProps> = ({ onLogout }) => {
   return (
-    <>
-      {/* Common Header for all tabs */}
-      <Header />
+    <IonTabs>
+      <IonRouterOutlet>
+        <Route exact path="/home" component={Homepage} />
+        <Route exact path="/history" component={History} />
+        <Route exact path="/profile">
+          <Profile onLogout={onLogout} />
+        </Route>
+        <Route exact path="/">
+          <Redirect to="/home" />
+        </Route>
+      </IonRouterOutlet>
 
-      <IonTabs>
-        <IonRouterOutlet>
-          {/* Tab Routes */}
-          <Route exact path="/home" component={Homepage} />
-          <Route exact path="/history" component={History} />
-          <Route exact path="/profile">
-            <Profile onLogout={onLogout} />
-          </Route>
+      <IonTabBar slot="bottom" color="light">
+        <IonTabButton tab="home" href="/home">
+          <IonIcon icon={homeOutline} />
+          <IonLabel>Home</IonLabel>
+        </IonTabButton>
 
-          {/* Default redirect to /home */}
-          <Route exact path="/">
-            <Redirect to="/home" />
-          </Route>
+        <IonTabButton tab="history" href="/history">
+          <IonIcon icon={documentOutline} />
+          <IonLabel>History</IonLabel>
+        </IonTabButton>
 
-          {/* Catch-all fallback */}
-          <Route>
-            <Redirect to="/home" />
-          </Route>
-        </IonRouterOutlet>
-
-        {/* Bottom Tab Bar */}
-        <IonTabBar slot="bottom" color="light">
-          <IonTabButton tab="home" href="/home">
-            <IonIcon icon={homeOutline} />
-            <IonLabel>Home</IonLabel>
-          </IonTabButton>
-
-          <IonTabButton tab="history" href="/history">
-            <IonIcon icon={documentOutline} />
-            <IonLabel>History</IonLabel>
-          </IonTabButton>
-
-          <IonTabButton tab="profile" href="/profile">
-            <IonIcon icon={personOutline} />
-            <IonLabel>Profile</IonLabel>
-          </IonTabButton>
-        </IonTabBar>
-      </IonTabs>
-    </>
+        <IonTabButton tab="profile" href="/profile">
+          <IonIcon icon={personOutline} />
+          <IonLabel>Profile</IonLabel>
+        </IonTabButton>
+      </IonTabBar>
+    </IonTabs>
   );
 };
 
